9dda8f0273c58ddd272affd1f3a58c19d4637f08
jalexpr <alex.porechny@mail.ru>
2020-04-18 18:27:07 +0200

    0.0.1 morphological-dictionary

---
+++ src/main/java/ru/textanalysis/tawt/md/Application.java
@@ -0,0 +1,7 @@
+package ru.textanalysis.tawt.md;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Словарь для morphological-structures");
+    }
+}
---
+++ src/main/java/ru/textanalysis/tawt/md/ResourcesUtils.java
@@ -0,0 +1,9 @@
+package ru.textanalysis.tawt.md;
+
+import java.io.InputStream;
+
+public class ResourcesUtils {
+    public static InputStream getResource(String pathZipFile) {
+        return ResourcesUtils.class.getClassLoader().getResourceAsStream(pathZipFile);
+    }
+}
